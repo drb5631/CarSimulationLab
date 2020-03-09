deb30db3508a420b0f09821a8c8158ae89e3358d
dblon <dblon@nextgen.com>
2020-03-09 15:19:03 -0400

    Added functionality for buttons

--- CarSimulationLab/MainWindow.xaml.cs
+++ CarSimulationLab/MainWindow.xaml.cs
@@ -20,9 +20,73 @@ namespace CarSimulationLab
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string domeLightState = "off";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DomeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+                switch (domeLightState)
+                {
+                    case "off":
+                        domeLightButton.Background = Brushes.Yellow;
+                        domeLightButton.Content = "Light On";
+                        domeLightState = "on";
+                        break;
+                    case "on":
+                        domeLightButton.Background = Brushes.Blue;
+                        domeLightButton.Content = "Light Auto";
+                        domeLightState = "auto";
+                        break;
+                    case "auto":
+                        domeLightButton.Background = Brushes.Black;
+                        domeLightButton.Content = "Light Off";
+                        domeLightState = "off";
+                        break;
+                }
+        }
+
+        private void BeltButton_Click(object sender, RoutedEventArgs e)
+        {
+            beltButton.
+
+            if ((string) beltButton.Content == "Not Buckled")
+            {
+                beltButton.Content = "Buckled";
+            }
+            else
+            {
+                beltButton.Content = "Not Buckled";
+            }
+        }
+
+        private void DriverSeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string) driverSeatButton.Content == "Sit") {
+                driverSeatButton.Content = "Driver Seated";
+            }
+            else
+            {
+                driverSeatButton.Content = "Sit";
+            }
+        }
+
+        private void BrakeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string) brakeButton.Content == "Apply Brake") {
+                brakeButton.Background = Brushes.Red;
+                brakeButton.Content = "Release Brake";
+            }
+            else
+            {
+                brakeButton.Background = Brushes.Black;
+                brakeButton.Content = "Apply Brake";
+            }
         }
     }
 }
