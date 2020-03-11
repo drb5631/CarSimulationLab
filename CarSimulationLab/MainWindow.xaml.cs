50b30d9c8f129291d774097293deaff287a8f268
dblon <dblon@nextgen.com>
2020-03-10 21:59:07 -0400

    FINAL PUSH - PROJECT COMPLETE

--- CarSimulationLab/MainWindow.xaml.cs
+++ CarSimulationLab/MainWindow.xaml.cs
@@ -31,41 +31,42 @@ namespace CarSimulationLab
         private void DomeButton_Click(object sender, RoutedEventArgs e)
         {
 
-                switch (domeLightState)
-                {
-                    case "off":
+            switch (domeLightState)
+            {
+                case "off":
+                    domeLightImage.Source = new BitmapImage(new Uri("images/domeLight.jpg", UriKind.Relative));
+                    domeLightButton.Background = Brushes.Yellow;
+                    domeLightButton.Foreground = Brushes.Black;
+                    domeLightButton.Content = "Light On";
+                    domeLightState = "on";
+                    break;
+                case "on":
+                    if (doorOpenButton.IsChecked == true)
+                    {
                         domeLightImage.Source = new BitmapImage(new Uri("images/domeLight.jpg", UriKind.Relative));
-                        domeLightButton.Background = Brushes.Yellow;
-                        domeLightButton.Foreground = Brushes.Black;
-                        domeLightButton.Content = "Light On";
-                        domeLightState = "on";
-                        break;
-                    case "on":
-                        if (doorOpenButton.IsChecked == true) {
-                        domeLightImage.Source = new BitmapImage(new Uri("images/domeLight.jpg", UriKind.Relative));
-                        }
-                        else
+                    }
+                    else
                     {
                         domeLightImage.Source = null;
                     }
-                        domeLightButton.Background = Brushes.Blue;
-                        domeLightButton.Content = "Light Auto";
-                        domeLightState = "auto";
-                        break;
-                    case "auto":
-                        domeLightImage.Source = null;
-                        domeLightButton.Background = Brushes.Black;
-                        domeLightButton.Foreground = Brushes.White;
-                        domeLightButton.Content = "Light Off";
-                        domeLightState = "off";
-                        break;
-                }
+                    domeLightButton.Background = Brushes.Blue;
+                    domeLightButton.Content = "Light Auto";
+                    domeLightState = "auto";
+                    break;
+                case "auto":
+                    domeLightImage.Source = new BitmapImage(new Uri("images/domeOff.jpeg", UriKind.Relative));
+                    domeLightButton.Background = Brushes.Black;
+                    domeLightButton.Foreground = Brushes.White;
+                    domeLightButton.Content = "Light Off";
+                    domeLightState = "off";
+                    break;
+            }
         }
 
         private void BeltButton_Click(object sender, RoutedEventArgs e)
         {
 
-            if ((string) beltButton.Content == "Not Buckled")
+            if ((string)beltButton.Content == "Not Buckled")
             {
                 beltButton.Background = Brushes.Green;
                 beltButton.Content = "Buckled";
@@ -81,7 +82,8 @@ namespace CarSimulationLab
 
         private void DriverSeatButton_Click(object sender, RoutedEventArgs e)
         {
-            if ((string) driverSeatButton.Content == "Sit") {
+            if ((string)driverSeatButton.Content == "Sit")
+            {
 
                 airBagImage.Source = new BitmapImage(new Uri("images/airbag.jpg", UriKind.Relative));
                 driverSeatButton.Background = Brushes.Green;
@@ -97,7 +99,8 @@ namespace CarSimulationLab
 
         private void BrakeButton_Click(object sender, RoutedEventArgs e)
         {
-            if ((string) brakeButton.Content == "Apply Brake") {
+            if ((string)brakeButton.Content == "Apply Brake")
+            {
 
                 brakeImage.Source = new BitmapImage(new Uri("images/brakeLights.jpg", UriKind.Relative));
                 brakeButton.Background = Brushes.Red;
@@ -113,7 +116,7 @@ namespace CarSimulationLab
 
         private void WindowButton_Click(object sender, RoutedEventArgs e)
         {
-            if ((string) windowButton.Content == "Window Up")
+            if ((string)windowButton.Content == "Window Up")
             {
                 windowImage.Source = new BitmapImage(new Uri("images/windowDown.jpg", UriKind.Relative));
                 windowButton.Background = Brushes.SkyBlue;
@@ -145,7 +148,7 @@ namespace CarSimulationLab
 
         private void WipersButton_Click(object sender, RoutedEventArgs e)
         {
-            if ((string) wipersButton.Content == "Wipers Off")
+            if ((string)wipersButton.Content == "Wipers Off")
             {
                 wipersImage.Source = new BitmapImage(new Uri("images/WipersOn.jpg", UriKind.Relative));
                 wipersButton.Background = Brushes.Green;
@@ -161,7 +164,7 @@ namespace CarSimulationLab
 
         private void HeadLightsButton_Click(object sender, RoutedEventArgs e)
         {
-            if ((string) headLightsButton.Content == "Lights Off")
+            if ((string)headLightsButton.Content == "Lights Off")
             {
                 headLightsImage.Source = new BitmapImage(new Uri("images/lightsOn.jpg", UriKind.Relative));
                 headLightsButton.Background = Brushes.Green;
